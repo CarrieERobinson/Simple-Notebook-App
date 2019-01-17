50b6c085dd4821155af657212f025c6f1868864d
Carrie <cerobi1@ilstu.edu>
2019-01-17 12:51:25 -0600

    Adapter is working as is click listener. All elements of data list are not showing up yet.

--- app/src/main/java/edu/ilstu/cerobi1/notepad/MainActivity.java
+++ app/src/main/java/edu/ilstu/cerobi1/notepad/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.ilstu.cerobi1.notepad;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,8 +16,10 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyAdapter.ItemClickListener {
 
+    MyAdapter adapter;
+    ArrayList<String> noteList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +27,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final String listFile = "NotesDir";
-        ArrayList<String> noteList = new ArrayList<>();
+        noteList = new ArrayList<>();
+        noteList.add("Item 1");
+        noteList.add("Item 2");
 
         //read from save file
-        try {
+        /*try {
             FileInputStream fileInputStream = openFileInput(listFile);
             int i;
             String temp = "";
@@ -47,10 +52,10 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception e)
         {
             e.printStackTrace();
-        }
+        }*/
 
 
-
+        //Set up recycleview
         RecyclerView rView = findViewById(R.id.rView);
 
         rView.setHasFixedSize(true);
@@ -58,12 +63,21 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager lManager = new LinearLayoutManager(this);
         rView.setLayoutManager(lManager);
 
-        RecyclerView.Adapter adapter = new MyAdapter(noteList);
+        adapter = new MyAdapter(this, noteList);
+        adapter.setClickListener(this);
         rView.setAdapter(adapter);
 
 
+    }
 
+    //toast for debugging purposes right now, later add in intent to go to note that is clicked
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
 
+        Intent intent = new Intent();
+        intent.putExtra("TITLE", noteList.get(position));
+        
     }
 }
 
--- app/src/main/java/edu/ilstu/cerobi1/notepad/MyAdapter.java
+++ app/src/main/java/edu/ilstu/cerobi1/notepad/MyAdapter.java
@@ -1,11 +1,13 @@
 package edu.ilstu.cerobi1.notepad;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,6 +24,8 @@ public class MyAdapter extends RecyclerView.Adapter {
     //Layout inflater: not sure what it does, but I guess I need it
     private LayoutInflater layoutInflater;
 
+    private ItemClickListener clickListener;
+
     //Constructor: takes in context and the list of notes the user has saved
     public MyAdapter(Context c, ArrayList<String> dataset)
     {
@@ -29,22 +33,54 @@ public class MyAdapter extends RecyclerView.Adapter {
         this.layoutInflater = LayoutInflater.from(c);
     }
 
-    
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = layoutInflater.inflate(R.layout.r_view_row, viewGroup, false);
-        return new RecyclerView.ViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+        String noteTitle = dataset.get(i);
+        ViewHolder vH = (ViewHolder) viewHolder;
+        ((ViewHolder) viewHolder).myTextView.setText(noteTitle);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataset.size();
     }
 
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.item_text);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener != null) clickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return dataset.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.clickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
 }
